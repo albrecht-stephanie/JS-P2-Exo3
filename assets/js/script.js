40b7c7f95214baedd495b68936f25028f5e73c85
AS <albrecht.stephanie@outlook.fr>
2019-10-28 22:20:15 +0100

    correctif

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+document.getElementById('lastname').addEventListener('keyup',function(){
+   var lastname = document.getElementById('lastname').value;
+   alert(lastname);
+});
+/* on récupère par id le contenu du champ lastname et lui ajout l'évènement keyup : lors appuie sur une touch.
+on récupère la value du champ et l'affiche dans une alerte*/
